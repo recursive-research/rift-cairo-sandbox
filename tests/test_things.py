5b522066d3e3194b63bce7f2b33ef27256a3eb2c
Michael Rodriguez <mike@recursivelabs.org>
2022-07-13 15:49:11 -0400

    test fight failure cases

--- tests/test_things.py
+++ tests/test_things.py
@@ -1,7 +1,6 @@
 """contract.cairo test file."""
 import os
 
-
 import pytest
 from starkware.starknet.testing.starknet import Starknet, StarknetContract
 from signers import MockSigner
@@ -53,22 +52,17 @@ async def deploy_thing(sn: Starknet):
     )
 
 
-# The testing library uses python's asyncio. So the following
-# decorator and the ``async`` keyword are needed.
 @pytest.mark.asyncio
 async def test_owner():
     """Test owner method."""
-    # Create a new Starknet class that simulates the StarkNet
-    # system.
+
     starknet = await Starknet.empty()
 
     # Deploy the contract.
     contract = await deploy_thing(starknet)
 
-    # Invoke increase_balance() twice.
     data = await contract.owner().call()
     assert data.result.owner == OWNER
-    # Check the result of get_balance().
 
 
 @pytest.mark.asyncio
@@ -140,3 +134,79 @@ async def test_fight():
 
     assert sorted([MSG_LOST, MSG_WON]) == sorted(
         [fightResult1Str, fightResult2Str])
+
+
+@pytest.mark.asyncio
+async def test_fight_non_owner():
+    """Test fight method called by non-owner of thing."""
+
+    starknet = await Starknet.empty()
+
+    contract = await deploy_thing(starknet)
+
+    THING_1 = 'thing1'
+    THING_2 = 'thing2'
+
+    signer1 = MockSigner(string_to_int(THING_1))
+    signer2 = MockSigner(string_to_int(THING_2))
+
+    account1 = await account(starknet, signer1)
+    account2 = await account(starknet, signer2)
+    mintResult1 = await mint_thing(
+        signer1,
+        contract,
+        account1,
+        THING_1
+    )
+
+    mintResult2 = await mint_thing(
+        signer2,
+        contract,
+        account2,
+        THING_2
+    )
+    with pytest.raises(Exception):
+        await fight_thing(
+            signer1,
+            contract,
+            account1,
+            mintResult2.result.response[0],
+            mintResult1.result.response[0]
+        )
+
+
+@pytest.mark.asyncio
+async def test_fight_same_owner():
+    """Test fight method with two things of the same owner."""
+
+    starknet = await Starknet.empty()
+
+    contract = await deploy_thing(starknet)
+
+    THING_1 = 'thing1'
+    THING_2 = 'thing2'
+
+    signer1 = MockSigner(string_to_int(THING_1))
+    account1 = await account(starknet, signer1)
+
+    mintResult1 = await mint_thing(
+        signer1,
+        contract,
+        account1,
+        THING_1
+    )
+
+    mintResult2 = await mint_thing(
+        signer1,
+        contract,
+        account1,
+        THING_2
+    )
+    with pytest.raises(Exception):
+        await fight_thing(
+            signer1,
+            contract,
+            account1,
+            mintResult1.result.response[0],
+            mintResult2.result.response[0]
+        )
